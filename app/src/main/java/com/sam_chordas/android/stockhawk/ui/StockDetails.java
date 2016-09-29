7e10571c552c03c1135e2bd1016c2e8238c31d13
Aysia <asia.lv@hotmail.com>
2016-09-29 13:22:21 -0700

    Added StockDetails

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetails.java
@@ -0,0 +1,10 @@
+package com.sam_chordas.android.stockhawk.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Lani on 9/29/2016.
+ */
+
+public class StockDetails extends AppCompatActivity{
+}
